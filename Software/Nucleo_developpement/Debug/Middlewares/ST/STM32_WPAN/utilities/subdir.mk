eb6fbed14ea0b3ffeb1a06fbea5d3ec9ca931960
AurelienLesage <2001aurelien.lesage@gmail.com>
2023-11-09 15:26:15 +0100

    Add test software
    - An example code of a SPI communication between a Raspberry and stm32 programs to send and receive a "Hello"

---
+++ Software/Nucleo_developpement/Debug/Middlewares/ST/STM32_WPAN/utilities/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Middlewares/ST/STM32_WPAN/utilities/dbg_trace.c \
+../Middlewares/ST/STM32_WPAN/utilities/otp.c \
+../Middlewares/ST/STM32_WPAN/utilities/stm_list.c \
+../Middlewares/ST/STM32_WPAN/utilities/stm_queue.c 
+
+OBJS += \
+./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.o \
+./Middlewares/ST/STM32_WPAN/utilities/otp.o \
+./Middlewares/ST/STM32_WPAN/utilities/stm_list.o \
+./Middlewares/ST/STM32_WPAN/utilities/stm_queue.o 
+
+C_DEPS += \
+./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.d \
+./Middlewares/ST/STM32_WPAN/utilities/otp.d \
+./Middlewares/ST/STM32_WPAN/utilities/stm_list.d \
+./Middlewares/ST/STM32_WPAN/utilities/stm_queue.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Middlewares/ST/STM32_WPAN/utilities/%.o Middlewares/ST/STM32_WPAN/utilities/%.su Middlewares/ST/STM32_WPAN/utilities/%.cyclo: ../Middlewares/ST/STM32_WPAN/utilities/%.c Middlewares/ST/STM32_WPAN/utilities/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WB55xx -DZIGBEE_WB -c -I../Core/Inc -I../Drivers/STM32WBxx_HAL_Driver/Inc -I../Drivers/STM32WBxx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32WBxx/Include -I../Drivers/CMSIS/Include -I../STM32_WPAN/App -I../Utilities/lpm/tiny_lpm -I../Middlewares/ST/STM32_WPAN -I../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread -I../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl -I../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci -I../Middlewares/ST/STM32_WPAN/utilities -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/extras -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zgp -I../Utilities/sequencer -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/key -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/se -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/security -I../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general -I../Middlewares/ST/STM32_WPAN/zigbee/platform -I../Middlewares/ST/STM32_WPAN/zigbee/core/inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Middlewares-2f-ST-2f-STM32_WPAN-2f-utilities
+
+clean-Middlewares-2f-ST-2f-STM32_WPAN-2f-utilities:
+	-$(RM) ./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.cyclo ./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.d ./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.o ./Middlewares/ST/STM32_WPAN/utilities/dbg_trace.su ./Middlewares/ST/STM32_WPAN/utilities/otp.cyclo ./Middlewares/ST/STM32_WPAN/utilities/otp.d ./Middlewares/ST/STM32_WPAN/utilities/otp.o ./Middlewares/ST/STM32_WPAN/utilities/otp.su ./Middlewares/ST/STM32_WPAN/utilities/stm_list.cyclo ./Middlewares/ST/STM32_WPAN/utilities/stm_list.d ./Middlewares/ST/STM32_WPAN/utilities/stm_list.o ./Middlewares/ST/STM32_WPAN/utilities/stm_list.su ./Middlewares/ST/STM32_WPAN/utilities/stm_queue.cyclo ./Middlewares/ST/STM32_WPAN/utilities/stm_queue.d ./Middlewares/ST/STM32_WPAN/utilities/stm_queue.o ./Middlewares/ST/STM32_WPAN/utilities/stm_queue.su
+
+.PHONY: clean-Middlewares-2f-ST-2f-STM32_WPAN-2f-utilities
+
---
+++ Software/Nucleo_developpement/Release/STM32_WPAN/App/app_zigbee.d
@@ -0,0 +1,198 @@
+STM32_WPAN/App/app_zigbee.o: ../STM32_WPAN/App/app_zigbee.c \
+ ../Core/Inc/app_common.h ../Core/Inc/app_conf.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/hw.h \
+ ../Core/Inc/hw_conf.h ../Core/Inc/hw_if.h \
+ ../Drivers/CMSIS/Device/ST/STM32WBxx/Include/stm32wbxx.h \
+ ../Drivers/CMSIS/Device/ST/STM32WBxx/Include/stm32wb55xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32WBxx/Include/system_stm32wbxx.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal.h \
+ ../Core/Inc/stm32wbxx_hal_conf.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_dma.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_def.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_dma.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_dmamux.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_dma_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_cortex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_exti.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_flash.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_flash_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_gpio.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_gpio_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_hsem.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_i2c.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_i2c_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pcd.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_usb.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pcd_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pwr.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_pwr.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_exti.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pwr_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rcc.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rcc.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_bus.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rcc_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_crs.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rtc.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rtc_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_spi.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_spi_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_uart.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_uart_ex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_system.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_exti.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_system.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rcc.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_ipcc.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_bus.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_pwr.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_cortex.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_utils.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_hsem.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_gpio.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rtc.h \
+ ../Core/Inc/app_entry.h \
+ ../Middlewares/ST/STM32_WPAN/utilities/dbg_trace.h \
+ ../STM32_WPAN/App/app_zigbee.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h \
+ ../Middlewares/ST/STM32_WPAN/stm32_wpan_common.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl_zigbee_hci.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_core.h \
+ ../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_def.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/stm32wbxx_core_interface_def.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_types.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_interface.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/stm32wbxx_core_interface_def.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/pletoh.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/llist.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/ieee802154_enums.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/mcp_enums.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.security.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.hash.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.bdb.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.aps.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.nwk.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.startup.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.touchlink.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.enum.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.zdo.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci/shci.h \
+ ../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/mbox_def.h \
+ ../Core/Inc/stm_logging.h ../Core/Inc/app_conf.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_types.h \
+ ../Utilities/sequencer/stm32_seq.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.temp.meas.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.elec.meas.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.fan.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.onoff.h \
+ ../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.illum.meas.h
+../Core/Inc/app_common.h:
+../Core/Inc/app_conf.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/hw.h:
+../Core/Inc/hw_conf.h:
+../Core/Inc/hw_if.h:
+../Drivers/CMSIS/Device/ST/STM32WBxx/Include/stm32wbxx.h:
+../Drivers/CMSIS/Device/ST/STM32WBxx/Include/stm32wb55xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32WBxx/Include/system_stm32wbxx.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal.h:
+../Core/Inc/stm32wbxx_hal_conf.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_dma.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_def.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_dma.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_dmamux.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_dma_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_cortex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_exti.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_flash.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_flash_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_gpio.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_gpio_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_hsem.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_i2c.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_i2c_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pcd.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_usb.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pcd_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pwr.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_pwr.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_exti.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_pwr_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rcc.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rcc.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_bus.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rcc_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_crs.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rtc.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_rtc_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_spi.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_spi_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_uart.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_uart_ex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_system.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_exti.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_system.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rcc.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_ipcc.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_bus.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_pwr.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_cortex.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_utils.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_hsem.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_gpio.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_ll_rtc.h:
+../Core/Inc/app_entry.h:
+../Middlewares/ST/STM32_WPAN/utilities/dbg_trace.h:
+../STM32_WPAN/App/app_zigbee.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h:
+../Middlewares/ST/STM32_WPAN/stm32_wpan_common.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl_zigbee_hci.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_core.h:
+../Drivers/STM32WBxx_HAL_Driver/Inc/stm32wbxx_hal_def.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/stm32wbxx_core_interface_def.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_types.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/tl.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_interface.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/stm32wbxx_core_interface_def.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/pletoh.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/llist.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/ieee802154_enums.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/mac/mcp_enums.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.security.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.hash.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.bdb.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.aps.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.nwk.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.startup.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.touchlink.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.enum.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zigbee.zdo.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/shci/shci.h:
+../Middlewares/ST/STM32_WPAN/interface/patterns/ble_thread/tl/mbox_def.h:
+../Core/Inc/stm_logging.h:
+../Core/Inc/app_conf.h:
+../Middlewares/ST/STM32_WPAN/zigbee/core/inc/zigbee_types.h:
+../Utilities/sequencer/stm32_seq.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/zcl.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.temp.meas.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.elec.meas.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.fan.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.onoff.h:
+../Middlewares/ST/STM32_WPAN/zigbee/stack/include/zcl/general/zcl.illum.meas.h:
